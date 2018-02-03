5782847bee7728016abc67b6568f7889d70929f0
qiuyu <956318206@qq.com>
2018-02-03 22:04:33 +0800

    initial version for linux system programming error handling functions

---
+++ err_handlers/err_handlers.h
@@ -0,0 +1,29 @@
+#ifndef _ERR_HANDLERS_H
+#define _ERR_HANDLERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <limits.h>
+
+#define MAXLINE 	2048
+static void err_handle(int errnoflag, int error, const char *fmt, va_list ap);
+/* fatal error handlers */
+	/* unrelated to a system call */
+extern void err_quit(const char *fmt, ...);
+extern void err_quit_err(int error, const char *fmt, ...);
+	/* related to a system calll */
+extern void err_sys(const char *fmt, ...);
+extern void err_sys_exit(const char *fmt, ...);
+extern void err_dump(const char *fmt, ...);
+/* nonfatal error handlers */
+	/* unrelated to a system call */
+extern void err_msg(const char *fmt, ...);
+extern void err_msg_err(int error, const char *fmt, ...);
+	/* related to a system call */
+extern void err_ret(const char *fmt, ...);
+
+#endif /* _ERR_HANDLERS_H */
