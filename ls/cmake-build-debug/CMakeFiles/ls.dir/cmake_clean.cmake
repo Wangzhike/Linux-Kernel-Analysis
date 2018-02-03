7fe5b22059f13f1dc36a41cd9b6b96f478c7be71
qiuyu <956318206@qq.com>
2018-02-03 23:05:24 +0800

    other's implement for ls commond in Clion project

---
+++ ls/cmake-build-debug/CMakeFiles/ls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ls.dir/main.cpp.o"
+  "ls.pdb"
+  "ls"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
